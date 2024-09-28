559eeef566eb7d39acb236848a457ae80d482125
사용자 junyoung011019 <kidjjy5@naver.com>
2024-09-28 17:50:56 +0900

    cors 인증 추가

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path');
 const dotenv = require('dotenv');
 const https=require('https');
-const PORT=80
+const cors = require('cors');
 
 dotenv.config();
 
