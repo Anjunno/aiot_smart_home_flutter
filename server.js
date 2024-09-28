50978499099e63d5e17d04397bf251785d7e2235
사용자 junyoung011019 <kidjjy5@naver.com>
2024-09-28 17:52:00 +0900

    fs

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path');
 const dotenv = require('dotenv');
 const https=require('https');
+const fs=require('fs');
 const cors = require('cors');
 
 dotenv.config();
